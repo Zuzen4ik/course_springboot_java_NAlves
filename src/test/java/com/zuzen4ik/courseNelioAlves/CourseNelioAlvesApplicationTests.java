baad887aebc641934436ce83276a19c807fb11d0
Zuzen4ik <zuzen4ik@hotmail.com>
2021-03-28 23:32:50 +0100

    Project created

---
+++ src/test/java/com/zuzen4ik/courseNelioAlves/CourseNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuzen4ik.courseNelioAlves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseNelioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
