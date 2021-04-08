933f0f3119d5e2a8b3425fbdc55a14f657d55a53
Zuzen4ik <zuzen4ik@hotmail.com>
2021-04-09 00:02:05 +0100

    Payment, one-to-one association

--- src/main/java/com/zuzen4ik/courseNelioAlves/config/TestConfig.java
+++ src/main/java/com/zuzen4ik/courseNelioAlves/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.zuzen4ik.courseNelioAlves.entities.Category;
 import com.zuzen4ik.courseNelioAlves.entities.Order;
 import com.zuzen4ik.courseNelioAlves.entities.OrderItem;
+import com.zuzen4ik.courseNelioAlves.entities.Payment;
 import com.zuzen4ik.courseNelioAlves.entities.Product;
 import com.zuzen4ik.courseNelioAlves.entities.User;
 import com.zuzen4ik.courseNelioAlves.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 =new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
