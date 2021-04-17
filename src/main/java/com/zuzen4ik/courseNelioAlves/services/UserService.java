17e07f420feaf193cb0e95606e02b9f22bdbb97b
Zuzen4ik <zuzen4ik@hotmail.com>
2021-04-17 23:56:14 +0200

    exceptionHandling.findById

--- src/main/java/com/zuzen4ik/courseNelioAlves/services/UserService.java
+++ src/main/java/com/zuzen4ik/courseNelioAlves/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.zuzen4ik.courseNelioAlves.entities.User;
 import com.zuzen4ik.courseNelioAlves.repositories.UserRepository;
+import com.zuzen4ik.courseNelioAlves.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User FindById (Integer id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
